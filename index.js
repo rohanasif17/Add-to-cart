fbce903a906b99761acedefcac85701f33d539da
rohanasif17 <111883502+rohanasif17@users.noreply.github.com>
2023-06-30 21:59:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+    onValue,
+    remove
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://realtime-database-11775-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://realtime-database-11775-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 const app = initializeApp(appSettings);
@@ -16,60 +24,60 @@ const categoryEl = document.getElementById('category')
 let username = "";
 
 addButtonEl.addEventListener("click", function() {
-  let inputValue = inputFieldEl.value.trim();
-  const category = categoryEl.value
+    let inputValue = inputFieldEl.value.trim();
+    const category = categoryEl.value
 
-  if (username && inputValue) {
-    const newItem = {
-      item: inputValue,
-      category: category.trim()
-    };
-    
+    if (username && inputValue) {
+        const newItem = {
+            item: inputValue,
+            category: category.trim()
+        };
 
-    push(ref(database, `shoppingList/${username}`), newItem);
-    clearInputFieldEl();
-  }
+
+        push(ref(database, `shoppingList/${username}`), newItem);
+        clearInputFieldEl();
+    }
 });
 
 function loadShoppingListForUser(user) {
-  username = user;
-  onValue(ref(database, `shoppingList/${user}`), function(snapshot) {
-    if (snapshot.exists()) {
-      let shoppingList = snapshot.val();
-      clearShoppingListEl();
+    username = user;
+    onValue(ref(database, `shoppingList/${user}`), function(snapshot) {
+        if (snapshot.exists()) {
+            let shoppingList = snapshot.val();
+            clearShoppingListEl();
 
-      for (let itemID in shoppingList) {
-        let itemData = shoppingList[itemID];
-        appendItemToShoppingListEl(itemID, itemData);
-      }
-    } else {
-      clearShoppingListEl();
-      shoppingListEl.innerHTML = "No items here... yet";
-    }
-  });
+            for (let itemID in shoppingList) {
+                let itemData = shoppingList[itemID];
+                appendItemToShoppingListEl(itemID, itemData);
+            }
+        } else {
+            clearShoppingListEl();
+            shoppingListEl.innerHTML = "No items here... yet";
+        }
+    });
 }
 
 function clearShoppingListEl() {
-  shoppingListEl.innerHTML = "";
+    shoppingListEl.innerHTML = "";
 }
 
 function clearInputFieldEl() {
-  inputFieldEl.value = "";
- 
- 
+    inputFieldEl.value = "";
+
+
 }
 
 function appendItemToShoppingListEl(itemID, itemData) {
-  let newEl = document.createElement("li");
+    let newEl = document.createElement("li");
 
-  newEl.textContent = itemData.item + " : " + itemData.category;
+    newEl.textContent = itemData.item + " : " + itemData.category;
 
-  newEl.addEventListener("click", function() {
-    let exactLocationOfItemInDB = ref(database, `shoppingList/${username}/${itemID}`);
-    remove(exactLocationOfItemInDB);
-  });
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${username}/${itemID}`);
+        remove(exactLocationOfItemInDB);
+    });
 
-  shoppingListEl.append(newEl);
+    shoppingListEl.append(newEl);
 }
 
 const page = document.getElementById("container");
@@ -82,20 +90,20 @@ reqLine.style.display = "none";
 
 let submit = document.getElementById("submit");
 submit.addEventListener("click", function() {
-  const username = usernameInput.value.trim();
-  if (username === "") {
-    reqLine.style.display = "block";
-  } else {
-    usernameContainer.style.display = "none";
-    page.style.display = "block";
-    loadShoppingListForUser(username);
-  }
+    const username = usernameInput.value.trim();
+    if (username === "") {
+        reqLine.style.display = "block";
+    } else {
+        usernameContainer.style.display = "none";
+        page.style.display = "block";
+        loadShoppingListForUser(username);
+    }
 });
 
 addButtonEl.addEventListener('click', function(event) {
     event.preventDefault();
-   
-    inputFieldEl.value = '';
-   
 
-});       
+    inputFieldEl.value = '';
+
+
+});
